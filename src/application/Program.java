fe3d4f729936fc4f11d60d7bc9287ac79774a7aa
Francisco Chaves Vasconcelos de Lima Junior <99039492>
2025-07-02 11:28:52 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,8 +18,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== Test 1: seller findByID ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 	
